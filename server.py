78321334976c75509f4c190d19df5caf873dbf5f
shubhodeep9 <shubhodeep9@gmail.com>
2015-07-25 16:01:57 +0530

    json

--- server.py
+++ server.py
@@ -5,6 +5,8 @@ import requests
 import json
 import os
 import urllib2
+import simplejson as jsons
+from flask import Response
 
 app = Flask(__name__)
 
@@ -12,42 +14,54 @@ app.secret_key = os.urandom(24)
 
 @app.route('/api', methods=['GET','POST'])
 def api():
+    js = []
+    c=0
     img_url = request.args.get('img',0)
     url = 'http://apius.faceplusplus.com/v2/detection/detect?api_key=e2707513a30c55f950583457e8845ec1&api_secret=9cWd6oDOtFMmqhGT7mwPKphefakx52tI&url='+str(img_url)
     page = urllib2.urlopen(url)
     data = json.load(page)
-    gender= data['face'][0]['attribute']['gender']['value']
-    age = data['face'][0]['attribute']['age']['value']
-    def getAgeCategory():
-    	age = data['face'][0]['attribute']['age']['value']
-    	if(age>55):
-    		ageCategory = "Old Age"
-    	if(age>20 and age<56):
-    		ageCategory = "Youth"
-		if(age>12 and age<21):
-			ageCategory = "Teenager"
-		if(age<12):
-		    ageCategory = "Kids"
-		
-		return ageCategory
-    def rating():
-    	smiling = data['face'][0]['attribute']['smiling']['value']
-    	if(smiling>75):
-    		rate=5
-    	if(smiling>61 and smiling<76):
-    		rate=4
-    	if(smiling>50 and smiling<62):
-    		rate=3
-    	if(smiling>35 and smiling<15):
-    		rate=2
-    	elif(smiling<15):
-    		rate=1
-    	return rate
-    rate1 = rating()
-    ageCategory = getAgeCategory()
-    return jsonify(gender=gender, rating=rate1, ageCategory = ageCategory)
+    for i in range(len(data['face'])):
+        gender= data['face'][i]['attribute']['gender']['value']
+        age = data['face'][i]['attribute']['age']['value']
+        def getAgeCategory():
+        	age = data['face'][i]['attribute']['age']['value']
+        	if(age>55):
+        		ageCategory = "Old Age"
+        	if(age>20 and age<56):
+        		ageCategory = "Youth"
+    		if(age>12 and age<21):
+    			ageCategory = "Teenager"
+    		if(age<12):
+    		    ageCategory = "Kids"
+
+    		return ageCategory
+        def rating():
+        	smiling = data['face'][i]['attribute']['smiling']['value']
+        	if(smiling>75):
+        		rate=5
+        	if(smiling>61 and smiling<76):
+        		rate=4
+        	if(smiling>50 and smiling<62):
+        		rate=3
+        	if(smiling>35 and smiling<15):
+        		rate=2
+        	elif(smiling<15):
+        		rate=1
+        	return rate
+        rate1 = rating()
+        ageCategory = getAgeCategory()
+        r = json.dumps({'gender':gender, 'rating':rate1, 'ageCategory': ageCategory})
+        js.append(r)
+        c=c+1
+    return '{'+','.join(str(j) for j in js)+'}'
+
+@app.route('/')
+def see():
+    url = 'http://localhost:5000/api?img=http://www.uhv.edu/bus/Images/ThreePeopleInSuits.jpg'
+    page = urllib2.urlopen(url)
+    data = json.loads(da)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT',5000))		
+    port = int(os.environ.get('PORT',5000))
     ## keep the debug mode on in flask - it helps
     app.run(host='0.0.0.0', port= port, debug=True)
